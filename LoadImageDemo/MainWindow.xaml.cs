d37389f4fb2aa06591c25a8e6e56607bba91e71b
smhcpk <smhcpk@SMHCPK-RAZER13>
2020-09-04 08:36:30 +0900

    Load image Demo

---
+++ LoadImageDemo/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoadImageDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Uri uri = new Uri("https://pps2pcc.azurewebsites.net/printed/5e032db1-5d91-4ad2-a701-8ef13e59b9c7.png");
+            ImageBox.Source = new BitmapImage(uri);
+        }
+    }
+}
